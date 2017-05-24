5931cbfdc4d692edd4dfe71f8b2ab4761e36658c
Matías Barletta <mrb@lionix.com>
2017-05-24 16:06:01 -0600

    fix: adding vendor autload

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,7 +1,8 @@
 <?php
-require_once 'bootstrap.php';
 require_once 'config.php';
 $sugar_config['lx_dbcrmserver'] = 'localhost';
+require_once '../vendor/autoload.php';
+
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 // replace with file to your own project bootstrap
