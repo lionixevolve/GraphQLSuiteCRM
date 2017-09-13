778ebf656c3e8878b53547e266371d2bf9871abc
Matías Barletta <mrb@lionix.com>
2017-09-13 14:02:24 -0600

    feat: parent_case support

--- graphql/Schema/CaseType.php
+++ graphql/Schema/CaseType.php
@@ -7,122 +7,149 @@ use Youshido\GraphQL\Execution\ResolveInfo;
 
 require_once 'argsHelper.php';
 
-class CaseType extends AbstractObjectType   // extending abstract Object type
+class CallType extends AbstractObjectType   // extending abstract Object type
 {
     public function build($config)  // implementing an abstract function where you build your type
     {
-        foreach (argsHelper::entityArgsHelper('Cases') as $field => $type) {
+        foreach (argsHelper::entityArgsHelper('Calls') as $field => $type) {
                 $config->addField($field, $type);
         }
-        $config->addField('notes', [
-                'type' => new ListType(new NoteType()),
-                'args' => argsHelper::entityArgsHelper('Notes'),
+        $config->addField('created_user_details', [
+                'type' => new UserType(),
                 'resolve' => function ($value, array $args, ResolveInfo $info) {
-                    if (!empty($value['notes'])) {
-                        $args['id']=$value['notes'];
-                        return NotesListType::resolve($value, $args, $info);
+                    if (!empty($value['created_user_details'])) {
+                        $args['id']=$value['created_user_details'];
+                        return UserType::resolve($value, $args, $info);
                     } else {
                         return null;
                     }
                  },
          ]);
-         $config->addField('created_user_details', [
-                 'type' => new UserType(),
-                 'resolve' => function ($value, array $args, ResolveInfo $info) {
-                     if (!empty($value['created_user_details'])) {
-                         $args['id']=$value['created_user_details'];
-                         return UserType::resolve($value, $args, $info);
-                     } else {
-                         return null;
-                     }
-                  },
-          ]);
-         $config->addField('assigned_user_details',[
-                 'type' => new UserType(),
-                 'resolve' => function ($value, array $args, ResolveInfo $info) {
-                     if (!empty($value['assigned_user_details'])) {
-                         $args['id']=$value['assigned_user_details'];
-                         return UserType::resolve($value, $args, $info);
-                     } else {
-                         return null;
-                     }
-                  },
-          ]);
-         $config->addField('modified_user_details', [
-                 'type' => new UserType(),
-                 'resolve' => function ($value, array $args, ResolveInfo $info) {
-                     if (!empty($value['modified_user_details'])) {
-                         $args['id']=$value['modified_user_details'];
-                         return UserType::resolve($value, $args, $info);
-                     } else {
-                         return null;
-                     }
-                  },
-          ]);
-          $config->addField('case_updates',[
-                  'type' => new CaseupdatesListType(),
-                  'args' => argsHelper::entityArgsHelper('AOP_Case_Updates'),
-                  'resolve' => function ($value, array $args, ResolveInfo $info) {
-                      if (!empty($value['case_updates'])) {
-                          $args['id']=$value['case_updates'];
-                          return CaseupdatesListType::resolve($value, $args, $info);
-                      } else {
+        $config->addField('assigned_user_details',[
+                'type' => new UserType(),
+                'resolve' => function ($value, array $args, ResolveInfo $info) {
+                    if (!empty($value['assigned_user_details'])) {
+                        $args['id']=$value['assigned_user_details'];
+                        return UserType::resolve($value, $args, $info);
+                    } else {
+                        return null;
+                    }
+                 },
+         ]);
+        $config->addField('modified_user_details', [
+                'type' => new UserType(),
+                'resolve' => function ($value, array $args, ResolveInfo $info) {
+                    if (!empty($value['modified_user_details'])) {
+                        $args['id']=$value['modified_user_details'];
+                        return UserType::resolve($value, $args, $info);
+                    } else {
+                        return null;
+                    }
+                 },
+         ]);
+        $config->addField('parent_contact', [
+                    'type'     => new ContactType(),
+                    'args' => argsHelper::entityArgsHelper('Contacts'),
+                    'resolve' => function ($value, array $args, ResolveInfo $info) {
+                        if (!empty($value['parent_contact'])) {
+                            $args['ids']=$value['parent_contact'];
+                            return ContactType::resolve($value, $args, $info);
+                        } else {
+                            return null;
+                        }
+                   },
+           ]);
+        $config->addField('parent_account', [
+                    'type' => new AccountType(),
+                    'args' => argsHelper::entityArgsHelper('Accounts'),
+                    'resolve' => function ($value, array $args, ResolveInfo $info) {
+                         if (!empty($value['parent_account'])) {
+                             $args['ids']=$value['parent_account'];
+                             return AccountType::resolve($value, $args, $info);
+                         } else {
+                             return null;
+                         }
+                    },
+            ]);
+            $config->addField('parent_case', [
+                    'type'     => new CaseType(),
+                    'args' => argsHelper::entityArgsHelper('Cases'),
+                   'resolve' => function ($value, array $args, ResolveInfo $info) {
+                       file_put_contents($_SERVER["DOCUMENT_ROOT"]."/lx.log", PHP_EOL. date_format(date_create(),"Y-m-d H:i:s ")  .__FILE__ .":". __LINE__." -- ".print_r($value, 1).PHP_EOL, FILE_APPEND);
+                       if (!empty($value['parent_case'])) {
+                           $args['id']=$value['parent_case'];
+                           return CaseType::resolve($value, $args, $info);
+                       } else {
                           return null;
                       }
-                   },
-               ]);
-          $config->addField('calls',[
-                  'type' => new ListType(new CallType()),
-                  'args' => argsHelper::entityArgsHelper('Calls'),
-                  'resolve' => function ($value, array $args, ResolveInfo $info) {
-                      if (!empty($value['calls'])) {
-                          $args['id']=$value['calls'];
-                          return CallsListType::resolve($value, $args, $info);
-                      } else {
-                          return null;
-                      }
-                   },
-               ]);
+                     },
+              ]);
+        $config->addField('parent_opportunity', [
+                    'type' => new OpportunityType(),
+                    'args' => argsHelper::entityArgsHelper('Opportunities'),
+                    'resolve' => function ($value, array $args, ResolveInfo $info) {
+                         if (!empty($value['parent_opportunity'])) {
+                             $args['ids']=$value['parent_opportunity'];
+                             return OpportunityType::resolve($value, $args, $info);
+                         } else {
+                             return null;
+                         }
+                    },
+            ]);
         $config->addField('contacts',[
                     'type' => new ContactsListType(),
                     'args' => argsHelper::entityArgsHelper('Contacts'),
                     'resolve' => function ($value, array $args, ResolveInfo $info) {
                          if (!empty($value['contacts'])) {
-                             return ContactsListType::resolve($value, ['id' => $value['contacts']], $info);
+                             $args['ids']=$value['contacts'];
+                             return ContactsListType::resolve($value, $args, $info);
                          } else {
                              return null;
                          }
-                    },
+                     }
                 ]);
         $config->addField('accounts', [
                     'type' => new AccountsListType(),
                     'args' => argsHelper::entityArgsHelper('Accounts'),
                     'resolve' => function ($value, array $args, ResolveInfo $info) {
                          if (!empty($value['accounts'])) {
-                             return AccountsListType::resolve($value, ['id' => $value['accounts']], $info);
+                             $args['ids']=$value['accounts'];
+                             return AccountsListType::resolve($value, $args, $info);
                          } else {
                              return null;
                          }
                     },
                 ]);
-        $config->addField('account_details', [
-                    'type' => new AccountType(),
-                    'args' => argsHelper::entityArgsHelper('Accounts'),
+        $config->addField('opportunities', [
+                    'type' => new OpportunitiesListType(),
+                    'args' => argsHelper::entityArgsHelper('Opportunities'),
                     'resolve' => function ($value, array $args, ResolveInfo $info) {
-                         if (!empty($value['account_details'])) {
-                             return AccountType::resolve($value, ['id' => $value['account_details']], $info);
+                         if (!empty($value['opportunities'])) {
+                              $args['ids']=$value['opportunities'];
+                             return OpportunitiesListType::resolve($value, $args, $info);
                          } else {
                              return null;
                          }
                     },
                 ]);
-
+        $config->addField('notes', [
+                'type' => new ListType(new NoteType()),
+                'args' => argsHelper::entityArgsHelper('Notes'),
+                'resolve' => function ($value, array $args, ResolveInfo $info) {
+                    if (!empty($value['notes'])) {
+                         $args['ids']=$value['notes'];
+                        return NotesListType::resolve($value, $args, $info);
+                    } else {
+                        return null;
+                    }
+                 },
+         ]);
     }
-    private function retrieveCase($id, $info)
+    private function retrieveCall($id, $info)
     {
         global $sugar_config, $current_user;
-        $caseBea = BeanFactory::getBean('Cases');
-        $case = $caseBea->retrieve($id);
+        $callBean = BeanFactory::getBean('Calls');
+        $call = $callBean->retrieve($id);
         if($info!=null){
             $getFieldASTList=$info->getFieldASTList();
             $queryFields=[];
@@ -132,14 +159,14 @@ class CaseType extends AbstractObjectType   // extending abstract Object type
         }
 
         $module_arr = array();
-        if ($case->id && $case->ACLAccess('view')) {
-            $all_fields = $case->column_fields;
+        if ($call->id && $call->ACLAccess('view')) {
+            $all_fields = $call->column_fields;
             foreach ($all_fields as $field) {
-                if (isset($case->$field) && !is_object($case->$field)) {
-                    $case->$field = from_html($case->$field);
-                    $case->$field = preg_replace("/\r\n/", '<BR>', $case->$field);
-                    $case->$field = preg_replace("/\n/", '<BR>', $case->$field);
-                    $module_arr[$field] = $case->$field;
+                if (isset($call->$field) && !is_object($call->$field)) {
+                    $call->$field = from_html($call->$field);
+                    $call->$field = preg_replace("/\r\n/", '<BR>', $call->$field);
+                    $call->$field = preg_replace("/\n/", '<BR>', $call->$field);
+                    $module_arr[$field] = $call->$field;
                 }
             }
             if(isset($queryFields) && array_key_exists('created_user_details',$queryFields)){
@@ -155,62 +182,64 @@ class CaseType extends AbstractObjectType   // extending abstract Object type
             }
 
             switch ($module_arr['parent_type']) {
+                case 'Cases':
+                    $module_arr['parent_case'] = $module_arr['parent_id'];
+                    $module_arr['parent_contact'] = '';
+                    $module_arr['parent_account'] = '';
+                    $module_arr['parent_opportunity'] = '';
+                    break;
                 case 'Contacts':
                     $module_arr['parent_contact'] = $module_arr['parent_id'];
                     $module_arr['parent_account'] = '';
                     $module_arr['parent_opportunity'] = '';
+                    $module_arr['parent_case'] = '';
                     break;
                 case 'Accounts':
                     $module_arr['parent_account'] = $module_arr['parent_id'];
                     $module_arr['parent_contact'] = '';
                     $module_arr['parent_opportunity'] = '';
+                    $module_arr['parent_case'] = '';
                     break;
                 case 'Opportunities':
                     $module_arr['parent_opportunity'] = $module_arr['parent_id'];
                     $module_arr['parent_contact'] = '';
                     $module_arr['parent_account'] = '';
+                    $module_arr['parent_case'] = '';
                     break;
                 default:
                     $module_arr['parent_opportunity'] = '';
                     $module_arr['parent_contact'] = '';
                     $module_arr['parent_account'] = '';
+                    $module_arr['parent_case'] = '';
                     break;
 
                 }
-            if(isset($queryFields) && array_key_exists('calls',$queryFields)){
-                $module_arr['calls'] =  array();
-                foreach ($case->get_linked_beans('calls') as $call) {
-                    $module_arr['calls'][] = $call->id;
+                if(isset($queryFields) && array_key_exists('contacts',$queryFields)){
+                    $module_arr['contacts'] =  array();
+                    foreach ($call->get_linked_beans('contacts', 'Contact') as $contact) {
+                        $module_arr['contacts'][] = $contact->id;
+                    }
                 }
-            }
-            if(isset($queryFields) && array_key_exists('contacts',$queryFields)){
-                $module_arr['contacts'] =  array();
-                foreach ($case->get_linked_beans('contacts', 'Contact') as $contact) {
-                    $module_arr['contacts'][] = $contact->id;
+                if(isset($queryFields) && array_key_exists('accounts',$queryFields)){
+                    $module_arr['accounts'] =  array();
+                        foreach ($call->get_linked_beans('accounts', 'Account') as $account) {
+                            $module_arr['accounts'][] = $account->id;
+                        }
                 }
-            }
-            if(isset($queryFields) && array_key_exists('accounts',$queryFields)){
-                $module_arr['accounts'] =  array();
-                foreach ($case->get_linked_beans('accounts', 'Account') as $account) {
-                    $module_arr['accounts'][] = $account->id;
+                if(isset($queryFields) && array_key_exists('opportunities',$queryFields)){
+                    $module_arr['opportunities'] =  array();
+                    foreach ($call->get_linked_beans('opportunities', 'Opportunity') as $opportunity) {
+                        $module_arr['opportunities'][] = $opportunity->id;
+                    }
                 }
-            }
-            if(isset($queryFields) && array_key_exists('case_updates',$queryFields)){
-                $module_arr['case_updates'] =   array();
-                foreach ($case->get_linked_beans('aop_case_updates', 'AOP_Case_Updates') as $updates) {
-                    $module_arr['case_updates'][] = $updates->id;
+                if(isset($queryFields) && array_key_exists('notes',$queryFields)){
+                    $module_arr['notes'] =  array();
+                    foreach ($call->get_linked_beans('notes') as $note) {
+                        $module_arr['notes'][] = $note->id;
+                    }
                 }
-            }
 
-            if(isset($queryFields) && array_key_exists('notes',$queryFields)){
-                $module_arr['notes'] =  array();
-                foreach ($case->get_linked_beans('notes') as $note) {
-                    $module_arr['notes'][] = $note->id;
-                }
-            }
-            if(isset($queryFields) && array_key_exists('account_details',$queryFields)){
-                    $module_arr['account_details'] = $case->account_id;
-            }
+
             return $module_arr;
         } else {
             return null;
@@ -220,24 +249,24 @@ class CaseType extends AbstractObjectType   // extending abstract Object type
     public function resolve($value = null, $args = [], $info = null)  // implementing resolve function
     {
         if (isset($args['id']) && is_array($args['id'])) {
-            foreach ($args as $key => $caseId) {
-                if (isset($caseId) && is_array($caseId)) {
-                    foreach ($caseId as $key => $caseIdItem) {
-                        $resultArray[] = self::retrieveCase($caseIdItem,$info);
+            foreach ($args as $key => $callId) {
+                if (isset($callId) && is_array($callId)) {
+                    foreach ($callId as $key => $callIdItem) {
+                        $resultArray[] = self::retrieveCall($callIdItem,$info);
                     }
-                } elseif (!empty($caseId)) {
-                    $resultArray[] = self::retrieveCase($caseId,$info);
+                } elseif (!empty($callId)) {
+                    $resultArray[] = self::retrieveCall($callId,$info);
                 }
             }
 
             return $resultArray;
         } elseif (!empty($args['id'])) {
-            return self::retrieveCase($args['id'],$info);
+            return self::retrieveCall($args['id'],$info);
         }
     }
 
     public function getName()
     {
-        return 'Case';  // important to use the real name here, it will be used later in the Schema
+        return 'Call';  // important to use the real name here, it will be used later in the Schema
     }
 }
